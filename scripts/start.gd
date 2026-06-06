f40e60aa17b44b4f2540b7ffe9e6d16d21f3bf4e
simonM-b <slivwrm88@gmail.com>
2026-06-06 17:22:24 -0400

    made something work

---
+++ scripts/start.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func startGame(setPath):
+	GLOBAL.startPath = setPath
+	get_tree().change_scene_to_file("res://scenes/the_story_path.tscn")
+
+
+func _on_new_save_pressed() -> void:
+	startGame(GLOBAL.firstTimeStartPath)
