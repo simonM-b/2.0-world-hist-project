4a7155ac40082b01e6fd7c9a5b2f4dce3a47dde6
simonM-b <slivwrm88@gmail.com>
2026-06-13 15:17:18 -0400

    expanded timeline

---
+++ scenes/citation_text.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+var textToAdd = "error: loading text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$"citation text".text = textToAdd
--- scripts/global.gd
+++ scripts/global.gd
@@ -3,6 +3,29 @@ extends Node
 var startPath = null
 
 var firstTimeStartPath = "res://timelines/planeToNewZeland.dtl"
+var citationTextPreload = preload("res://scenes/citation_text.tscn")
+
+var citations = [
+	"Johnes, S. (2024).  Interior of a Wizz Air Airbus A321neo . Retrieved June 13, 2026, from https://www.flickr.com/photos/197077715@N03/53519875812/.",
+	"颐园居. (2024). Inside Tokyo International Airport Terminal 3. wikimedia commons. Retrieved June 13, 2026, from https://commons.wikimedia.org/wiki/File:Inside_Tokyo_International_Airport_Terminal_3_20240604.jpg.",
+	"MrSeabody. (2024). The entrance to Bay of Islands Airport, in Northland, New Zealand . wikimedia. Retrieved June 13, 2026, from https://commons.wikimedia.org/wiki/File:Bay_of_Islands_Airport_outside_2024.jpg."
+]
+
+func showCitationText(textID: int):
+	var citation = citationTextPreload.instantiate()
+	var container = get_tree().root.get_node("the story path").get_node("CanvasLayer").get_node("VBoxContainer")
+	if container:
+		killAllCitationText(container)
+		container.add_child(citation)
+		if textID <= len(citations):
+			citation.text = citations[textID]
+			return
+	print("something went wrong with the path of the text container. ", textID)
+
+func killAllCitationText(r):
+	for i in r.get_children():
+		if i.is_in_group("citation-text"):
+			i.queue_free()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
